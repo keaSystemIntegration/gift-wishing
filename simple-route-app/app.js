824c3c5561ee9377f8f992493a8ddf76ceb2576b
iga <iga@trifork.com>
2022-10-06 18:53:03 +0200

    simple reverse proxy with haproxy and nodejs express

--- simple-route-app/app.js
+++ simple-route-app/app.js
@@ -1,10 +1,12 @@
 import express from "express"
 
-const PORT =process.env.PORT ?? 8080
+const PORT =process.env.PORT ?? 80
 
 const app = express()
 
 app.get("/*", (req,res)=> {
+    console.log("this is a call for me :D")
+    console.log(process.env.PORT)
     res.send({message: `simple app on port: ${PORT}`})
 })
 
