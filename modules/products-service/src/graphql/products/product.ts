4c1311e03a1795f9e8e4f96d5dcf749bd122f315
Itai Gramse <itai0004@stud.kea.dk>
2022-11-27 20:11:22 +0100

    Add redis cache for products

--- modules/products-service/src/graphql/products/product.ts
+++ modules/products-service/src/graphql/products/product.ts
@@ -1,20 +1,40 @@
-import { Arg, Query, Resolver } from 'type-graphql'
+import { Arg, Directive, Query, Resolver } from 'type-graphql'
 
 import { Product } from '../../schema/resolvers/product-resolver'
 import { PrismaClient } from '../../../prisma/client'
+import { CacheHint } from '@apollo/cache-control-types/src'
 
 const prisma = new PrismaClient()
 
 @Resolver()
 export class ProductResolver {
     @Query(() => [Product])
-    async getProducts() {
+    @CacheControl({ maxAge: 60 })
+    async Products() {
         return prisma.products.findMany()
     }
     @Query(() => Product)
-    async getProduct(@Arg('id') id: string) {
+    @CacheControl({ maxAge: 60, scope: 'PUBLIC' })
+    async Product(@Arg('id') id: string) {
         const product = await prisma.products.findUnique({ where: { id } })
         if (product) return product
         else throw new Error('product not found')
     }
+}
+
+export function CacheControl({ maxAge, scope = 'PUBLIC' }: CacheHint) {
+    if (!maxAge && !scope) {
+        throw new Error('Missing maxAge or scope param for @CacheControl')
+    }
+
+    let sdl = '@cacheControl('
+    if (maxAge) {
+        sdl += `maxAge: ${maxAge}`
+    }
+    if (scope) {
+        sdl += ` scope: ${scope}`
+    }
+    sdl += ')'
+
+    return Directive(sdl)
 }
