f68f91684316d46e303924c58b4a4d19505e0625
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-11-28 23:11:11 +0100

    User Service Cookier Parser update

--- modules/user-service/src/app.ts
+++ modules/user-service/src/app.ts
@@ -1,27 +1,28 @@
-import express from 'express';
-import { friendRoutes } from './routes/friend-routes';
-import { inviteRoutes } from './routes/invite-routes';
-import { relationshipRoutes } from './routes/relationship-routes';
-import { userRoutes } from './routes/user-routes';
-import { emailServerUrl } from './services/email-server-service';
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import { friendRoutes } from './routes/friend-routes'
+import { inviteRoutes } from './routes/invite-routes'
+import { relationshipRoutes } from './routes/relationship-routes'
+import { userRoutes } from './routes/user-routes'
+import { emailServerUrl } from './services/email-server-service'
 //import "dotenv/config";
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT;
-app.use(express.json());
-app.use("/user", userRoutes);
-app.use("/friend", friendRoutes);
-app.use("/invite", inviteRoutes);
-app.use("/relationship", relationshipRoutes);
-
+const port = process.env.PORT
+app.use(express.json())
+app.use(cookieParser())
+app.use('/user', userRoutes)
+app.use('/friend', friendRoutes)
+app.use('/invite', inviteRoutes)
+app.use('/relationship', relationshipRoutes)
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
-});
+    res.send('Hello World!')
+})
 
 app.listen(port, () => {
-	console.log(`user-service listening on port ${port}`)
-	console.log("Environment = " + process.env.NODE_ENV);
-	console.log("Email server = " + emailServerUrl)
-});
+    console.log(`user-service listening on port ${port}`)
+    console.log('Environment = ' + process.env.NODE_ENV)
+    console.log('Email server = ' + emailServerUrl)
+})
