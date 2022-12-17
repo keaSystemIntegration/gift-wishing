baab3e52394532cb6589bbd67b19836ba015cc6f
Gabriel <gabriel_nicutari@yahoo.ro>
2022-12-17 18:59:41 +0100

    Add CORS config on Proxy

--- modules/wishlist-service/src/app.js
+++ modules/wishlist-service/src/app.js
@@ -1,35 +1,25 @@
-import express from 'express';
-import "dotenv/config";
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
 
-const app = express({
-  cors: {
-    origin: "*",
-  },
-});
+const app = express()
 
-const { WISHLIST_CONNECTION_URL, WISHLIST_SERVICE_PORT } = process.env;
+const { WISHLIST_CONNECTION_URL, WISHLIST_SERVICE_PORT } = process.env
 
-mongoose.connect(WISHLIST_CONNECTION_URL);
-const db = mongoose.connection;
+mongoose.connect(WISHLIST_CONNECTION_URL)
+const db = mongoose.connection
 
-db.on('error', (error) => console.log(error));
-db.once('open', () => console.log('Connected to Wishlist Mongo Database'));
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to Wishlist Mongo Database'))
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(express.json())
+app.use(cookieParser())
 
-import wishlistsRouter from './routes/wishlistRoutes.js';
-app.use('/', wishlistsRouter);
+import wishlistsRouter from './routes/wishlistRoutes.js'
+app.use('/', wishlistsRouter)
 
-const PORT = WISHLIST_SERVICE_PORT || 8001;
+const PORT = WISHLIST_SERVICE_PORT || 8001
 app.listen(PORT, () => {
-  console.log(`WISHLIST server is running on PORT: ${PORT}`);
+    console.log(`WISHLIST server is running on PORT: ${PORT}`)
 })
